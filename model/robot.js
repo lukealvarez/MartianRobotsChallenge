fa1cc8b2dd3a85031c815fe8d9806bf6f14681a5
Luke Alvarez <lucas.alvarez@outlook.com>
2021-01-10 17:39:53 -0300

    add robot class

---
+++ model/robot.js
@@ -0,0 +1,9 @@
+class Robot {
+    constructor(positionX, positionY, orientation, instructions) {
+        this.coordinate = {positionX: positionX, positionY: positionY};
+        this.orientation = orientation;
+        this.instructions = instructions;
+    }
+
+    executeInstructions(){}
+}
