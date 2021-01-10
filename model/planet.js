2e06e8759949f4035e7a04e8b9b16de1a80dd9d3
Luke Alvarez <lucas.alvarez@outlook.com>
2021-01-10 17:47:18 -0300

    Add planet class

---
+++ model/planet.js
@@ -0,0 +1,11 @@
+class Planet {
+    constructor(name, maxX, maxY){
+        this.name = name;
+        this.coordinate = {positionX: maxX, positionY: maxY};
+        this.scents = [];
+    }
+
+    checkIfIsOff(coordinate){}
+
+    checkIfExistScent(coordinate){}
+}
